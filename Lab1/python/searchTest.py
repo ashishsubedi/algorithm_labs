2b5cd52a25f98676d717fdd525abc0e132c03766
Sailesh Dahal <saileshbro@gmail.com>
2020-08-02 18:56:38 +0545

    added test case to check if sorted or not

--- Lab1/python/searchTest.py
+++ Lab1/python/searchTest.py
@@ -8,25 +8,28 @@ class TestLinearSearchMethods(unittest.TestCase):
         data = [1, 2, 3, 4, 5]
         self.assertEqual(linear_search(data, 2), 1)
         self.assertEqual(linear_search(data, 10), -1)
+
     def testChar(self):
-        data = ['a','e','i','o','u']
+        data = ['a', 'e', 'i', 'o', 'u']
         self.assertEqual(linear_search(data, 'a'), 0)
         self.assertEqual(linear_search(data, 'z'), -1)
         self.assertEqual(linear_search(data, 'u'), 4)
-  
 
 
 class TestBinarySearch(unittest.TestCase):
     def testBinary(self):
-        data = [1, 2, 3, 4, 5]
-        self.assertEqual(binary_search(data, 2), 1)
+        data = [2, 1, 3, 4, 5]
+        self.assertEqual(data, sorted(data))
+        self.assertEqual(binary_search(data, 1), 1)
         self.assertEqual(binary_search(data, 10), -1)
 
     def testChar(self):
-        data = ['a','e','i','o','u']
+        data = ['a', 'e', 'i', 'o', 'u']
+        self.assertEqual(data, sorted(data))
         self.assertEqual(binary_search(data, 'a'), 0)
         self.assertEqual(binary_search(data, 'z'), -1)
         self.assertEqual(binary_search(data, 'u'), 4)
+
 
 if __name__ == "__main__":
     unittest.main()
